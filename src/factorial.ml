36ebfe97cadf744c22ef5fce0e2a35e96a05031c
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-03-30 19:10:27 -0300

    add new factorial func

--- src/factorial.ml
+++ src/factorial.ml
@@ -1,8 +1,11 @@
 (** Fatorial de um numero *)
-let rec factorial numero : int =
-    match numero with
+let rec factorial x : int =
+    match x with
     | 1 -> 1
     | n -> n * factorial (n - 1)
+
+let rec factorial2 x =
+    if x <= 1 then 1 else x * factorial2 (x - 1);;
 
 (*
 OCaml offers updatable memory cells, called references:
@@ -10,18 +13,25 @@ ref init returns a new cell with initial contents init,
 !cell returns the current contents of cell, and 
 cell := v writes the value v into cell.
 *)
-let factorial2 n =
+let factorial3 n =
     let result = ref 1 in
     for i = 2 to n do
       result := i * !result
     done;
     !result;;
 
+
 let () =
-    let f = factorial 3 in
+    let value = 5 in
+
+    let f = factorial value in
     print_int f;
     print_newline ();
 
-    let f = factorial2 5 in
+    let f = factorial2 value in
+    print_int f;
+    print_newline ();
+
+    let f = factorial3 value in
     print_int f;
     print_newline ();
